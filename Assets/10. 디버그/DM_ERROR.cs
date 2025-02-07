9dd9888538181399ac36bcf0e1d775e1d226f97c
1506022022 <88864717+1506022022@users.noreply.github.com>
2025-02-07 21:14:19 +0900

    feat(DM): 디버그 메시지 추가

---
+++ Assets/10. 디버그/DM_ERROR.cs
@@ -0,0 +1,4 @@
+public static class DM_ERROR
+{
+    public static readonly string OUT_OF_RANGE = "ii";
+}
---
+++ Assets/10. 디버그/DM_WARNING.cs
@@ -0,0 +1,7 @@
+/// <summary>
+/// Debug Message
+/// </summary>
+public static class DM_WARNING
+{
+    public static readonly string SERIALIZE_FIELD_EMPTY = "Please enter a reference in the field";
+}
